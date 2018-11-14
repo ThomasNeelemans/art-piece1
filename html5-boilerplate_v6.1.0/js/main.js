1dbc5a29f6efb69076f32f976701767f9c42dec8
Thomas Neelemans <thomas.neelemans@virtual-identity.com>
2018-11-14 16:59:21 +0100

    working header, working slideshow.

--- html5-boilerplate_v6.1.0/js/main.js
+++ html5-boilerplate_v6.1.0/js/main.js
@@ -3,19 +3,16 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 90 || document.documentElement.scrollTop  > 90) {
-    $(".menulink").addClass('menulink2');
     $('.headerbar').addClass('headerbar2');
     $('.search').addClass('search2');
-
+    $(".menulink").addClass('menulink2');
+   
   } 
   
   else {
-    $(".menulink").removeClass('menulink2');
-    $('.search').removeClass('search2');
     $('.headerbar').removeClass('headerbar2');
-
-    
-
+    $('.search').removeClass('search2');
+    $(".menulink").removeClass('menulink2');
   }
 }
 
@@ -28,3 +25,31 @@ $(window).scroll(function(){
 
 
 
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block"; 
+    dots[slideIndex-1].className += " active";
+  }
